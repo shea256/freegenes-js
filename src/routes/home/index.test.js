22c9004f977d90f973d969f27c27297c0404202c
Ryan Shea <ryan@shea.io>
2019-08-21 15:05:35 -0400

    complete simply home page test

--- src/routes/home/index.test.js
+++ src/routes/home/index.test.js
@@ -2,13 +2,14 @@
 /* eslint-disable padded-blocks, no-unused-expressions, no-underscore-dangle */
 
 import React from 'react';
-import renderer from 'react-test-renderer';
 import configureMockStore from 'redux-mock-store';
 import nodeFetch from 'node-fetch';
-
+import { mount } from 'enzyme';
 import App from '../../components/App';
 import indexAction from './index';
 import createFetch from '../../createFetch';
+
+// import renderer from 'react-test-renderer'
 
 const mockStore = configureMockStore();
 const baseUrl = 'http://localhost:3000';
@@ -17,28 +18,27 @@ describe('Home Page', () => {
   test('Renders correctly', async () => {
     const fetch = createFetch(nodeFetch, { baseUrl });
     const store = mockStore({});
+    const appContext = {
+      store,
+      insertCss: () => {},
+      fetch: () => {},
+      pathname: '/',
+    };
 
     const indexPage = await indexAction({ fetch, store })
       .then(value => value)
       .catch((/* err */) => {
-        // do nothing with the error
+        /* do nothing with error */
       });
 
-    const pageRenderer = await renderer.create(
-      <App
-        context={{
-          store,
-          insertCss: () => {},
-          fetch: () => {},
-          pathname: '/',
-        }}
-      >
-        {indexPage.component}
-      </App>,
+    const wrapper = await mount(
+      <App context={appContext}>{indexPage.component}</App>,
     );
-    const pageInstance = pageRenderer.root;
 
-    const h1 = pageInstance.findByType('h1');
-    expect(h1.children).toEqual(['The DNA Commons']);
+    const header = wrapper.find('h1');
+    expect(header.length).toEqual(1);
+
+    const cards = wrapper.find('.card');
+    expect(cards.length).toBeGreaterThan(0);
   });
 });
